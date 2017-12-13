0eb85971909b4f640bf22fff14659be46e42428c
Daniel S <daniel.shamailov05@gmail.com>
2017-12-12 19:59:47 -0500

    Updated Contact

--- root/Final_Project/contact/contact.js
+++ root/Final_Project/contact/contact.js
@@ -1,1 +1,8 @@
-contact.js
+// $('button').click(captureInput)
+
+// function captureInput() {
+//   event.preventDefault() //refreshes the page
+//  const input = $('input') // an element has props/methods
+//   const value = input.val()
+//   input.val('')
+// }
